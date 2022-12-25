e4fc24f20092ce001ad6754de8a512a7fdf0b199
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-25 21:33:45 +0900

    introduce react-beautiful-dnd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div style={{padding: "50px"}}>
       <h1>Apps like Trello</h1>
+      <Main />
     </div>
   );
 }
